3ef2ec487d9ff2d0e7b5c9e0f84f3409518b4b12
Hashaam Shahid <hashaamshahid111@gmail.com>
2025-07-15 21:41:25 +0500

    test69

--- .eleventy.js
+++ .eleventy.js
@@ -1,22 +1,21 @@
 const fs = require("fs");
 
 module.exports = function(eleventyConfig) {
-  // Pass-through copy
   eleventyConfig.addPassthroughCopy("src/assets/css");
   eleventyConfig.addPassthroughCopy("src/assets/js");
   eleventyConfig.addPassthroughCopy("src/assets/img");
   eleventyConfig.addPassthroughCopy({ "src/assets/img/favicon": "/" });
   eleventyConfig.addPassthroughCopy("src/assets/vid");
 
-  // ✅ Add collection for packages
-  eleventyConfig.addCollection("packages", function(collectionApi) {
+  eleventyConfig.addCollection("packages", function() {
     const dataPath = "./src/_data/packages.json";
-    const jsonData = JSON.parse(fs.readFileSync(dataPath, "utf-8"));
-    return jsonData.packages.map(pkg => ({
-      ...pkg,
-      url: `/packages/${pkg.slug}/`
-    }));
+    if (fs.existsSync(dataPath)) {
+      const jsonData = JSON.parse(fs.readFileSync(dataPath, "utf-8"));
+      return jsonData.packages || [];
+    }
+    return [];
   });
+
 
   return {
     dir: {
